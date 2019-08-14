49170ba5f0afedffab2d8879654e1866ce47eaf3
Salama Ashoush <salama.ashoush@brightcomputing.com>
2019-08-14 02:37:38 +0200

    initial commit

---
+++ apps/ngx-styled/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-styled-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngx-styled';
+}
